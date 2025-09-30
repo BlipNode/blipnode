739869e120ca23876a384ed529e9d66dbc906a2d
João Amgarten <joaoamgarten2003@gmail.com>
2025-09-30 11:37:00 +0100

    replace BrowserRouter to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter basename='/blipnode'>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 )
 
